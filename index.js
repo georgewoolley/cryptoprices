c6a4bc5148a97a7abf23cd8c452a3b8f83a826b5
Administrator <administrator@Administrators-MacBook-Pro.local>
2023-12-22 13:25:04 +0100

    Styling changes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import bodyParser from "body-parser";
 
 const app = express();
-const port = 3021;
+const port = 3032;
 const API_URL = "https://api.coinranking.com/v2/coins";
 
 // Set the view engine to EJS
